024a45aba9fb32f2e4d4e30d4aa47c51b72db8a4
jdfakd <jdfakd@naver.com>
2023-06-04 14:11:22 +0900

    변수 추가

---
+++ lib/aaaa/aeae.dart
@@ -0,0 +1,29 @@
+class Variable {
+
+  Variable(){
+
+      print("테스트");
+
+
+      //
+      String name = "박한별";
+      print(name);
+      int age = 32;
+      print(age);
+      double weight = 12.5;
+      print(weight);
+
+      bool check = false;
+      print(check);
+
+
+      age = 50;
+      print(age);
+
+
+
+
+
+  }
+
+}
