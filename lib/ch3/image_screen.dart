b9c397c3b349550f7f9f18c361e19326208ec96e
jdfakd <jdfakd@naver.com>
2023-06-18 17:12:24 +0900

    444444445555

--- lib/ch3/image_screen.dart
+++ lib/ch3/image_screen.dart
@@ -3,11 +3,32 @@ import 'package:flutter/material.dart';
 class ImageScreen extends StatelessWidget {
   const ImageScreen({super.key});
 
+  final String imageUrl =
+      "https://newsimg-hams.hankookilbo.com/2021/11/09/15423494-2006-403b-a665-6969a7b1aaa1.jpg";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("ImageScreen"),
+      ),
+      body: Column(
+        children: [
+          Image.asset("assets/cat.png"),
+
+          /// jpg, jpeg, png
+          /// png: 배경이 투명한 이미지
+          /// jpg, jpeg: 일반 이미지
+          Container(
+            color: Colors.red,
+            width: 200,
+            height: 200,
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ],
       ),
     );
   }
